f43356032dc01f83d46f67672c41e276bbc09346
Alex <alexbrochu1@gmail.com>
2021-11-11 01:53:23 -0500

    Got the data ordered better, ready to calc results. We can now determine the winner of a game. Returns winner and if we are in a tied match

--- controllers/pools.go
+++ controllers/pools.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+    "log"
     "FootPoolGo/services"
 
 	beego "github.com/beego/beego/v2/server/web"
@@ -18,18 +19,38 @@ type MatchData struct {
 
 func (c *PoolsController) Get() {
 
-    poolerId := c.GetSession("poolerId").(primitive.ObjectID)
+    /*
+    * Could use some channels to parallelize the work here.
+    */
+
+    //poolerId := c.GetSession("poolerId").(primitive.ObjectID)
     poolId := c.GetSession("poolId").(primitive.ObjectID)
 
-    matches := services.NflAPI.FetchMatches(2021, 9)
-    season, week := services.DB.FetchCurrentWeek(poolerId)
+    // Could be done together?
+    //season, week := services.DB.FetchCurrentWeek(poolerId)
+    season, week := 2021, 9
     poolers := services.DB.FetchPoolersFromPool(poolId)
+    // ------------------------------
 
+    // Could be done together?
+    matches := services.NflAPI.FetchMatches(season, week)
     poolPicks := services.DB.FetchPicks(poolers, season, week)
+    // ------------------------------
 
     poolData := make([]MatchData, len(matches))
     for i, match := range matches {
         poolData[i].Match = match
+
+        winner, isTie := GetWinner(&match)
+        log.Printf("[%v] > winner = %v [%v]\n", match.EventId, winner, isTie)
+
+        for _, pooler := range poolers {
+            log.Printf("[%v] > %v\n",
+                pooler.Name,
+                poolPicks[match.EventId][pooler.ID],
+            )
+        }
+        //TODO: Fill in results data
         poolData[i].Picks = poolPicks[match.EventId]
     }
 
@@ -43,5 +64,17 @@ func (c *PoolsController) Get() {
     c.Data["pooldata"] = poolData
 
     c.Render()
+}
+
+func GetWinner(m* services.Match) (string, bool) {
+    if m.AwayScore == m.HomeScore {
+        return "", true
+
+    } else if m.AwayScore > m.HomeScore {
+        return services.NflAPI.GetShortname(m.AwayTeam), false;
+
+    } else {
+        return services.NflAPI.GetShortname(m.HomeTeam), false;
+    }
 }
 
